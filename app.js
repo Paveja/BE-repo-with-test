7d3b8363394aa5ab98abdf80224af453f067c76a
sri.srisailam <sri.srisailam@rootquotient.com>
2025-10-06 17:23:00 +0530

    changed names

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/upload", async (req, res) => {
       return res.status(400).send("No file received");
     }
 
-    const bucketName = process.env.BUCKET_NAME
+    const bucketName = process.env.BUCKET_NAME ?? 'kendall-bucket-red-qa'
     const key = "apple.png";
 
     // Upload to S3
